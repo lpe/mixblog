8e35b78e94d4d06925947b6757ceba7cb1de429d
Louis-Philippe ENGELMANN <lph.engelmann@gmail.com>
2022-07-20 17:21:38 +0200

    Add routes : root / login / registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+
+  resources :login, only: [:new, :create]
+  resources :registration, only: [:new, :create]
+
 end
