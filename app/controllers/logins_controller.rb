62ff707d0299b56d7bbf03e9f128a0aee2269546
Louis-Philippe ENGELMANN <lph.engelmann@gmail.com>
2022-07-21 21:22:18 +0200

    Fix route error message after logout

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -17,7 +17,7 @@ class LoginsController < ApplicationController
 
   def destroy
     session[:current_user] = nil
-    redirect_to root_path
+    redirect_to root_path, status: :see_other
   end
 
   private
